ebd6ea2e51f79d51d4ce64d54477739b3baa2683
Luis Ramos <soporte@dante.pe>
2024-08-01 12:42:10 -0500

    actualizando

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -4,12 +4,11 @@ const Testimonials=()=> {
         <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
         <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
-          <img alt="" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" className="mx-auto h-12" />
+          <img alt="" src="" className="mx-auto h-12" />
           <figure className="mt-10">
             <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
               <p>
-                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+              "Como consultor en infraestructura TI, mi misión es diseñar sistemas escalables y seguros que optimicen el rendimiento y la continuidad del negocio, preparando tu empresa para los desafíos futuros y permitiéndote enfocarte en el crecimiento y la innovación."
               </p>
             </blockquote>
             <figcaption className="mt-10">
@@ -19,11 +18,12 @@ const Testimonials=()=> {
                 className="mx-auto h-10 w-10 rounded-full"
               />
               <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                <div className="font-semibold text-gray-900">Judith Black</div>
+                <div className="font-semibold text-gray-900">Luis Ramos Castilla</div>
                 <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
                   <circle r={1} cx={1} cy={1} />
                 </svg>
-                <div className="text-gray-600">CEO of Workcation</div>
+                <div className="text-gray-600">
+                IT Consultant</div>
               </div>
             </figcaption>
           </figure>
